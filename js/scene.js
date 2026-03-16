0c06aa069bc4169baa6a823787387547b66de8f8
Susan S. <200622750@student.georgianc.on.ca>
2026-03-16 07:09:55 -0400

    add initial scene setup with lighting in scene.js

---
+++ js/scene.js
@@ -0,0 +1,26 @@
+const canvas = document.getElementById("renderCanvas");
+
+const engine = new BABYLON.Engine(canvas, true);
+
+const createScene = async function () {
+    const scene = new BABYLON.Scene(engine);
+
+    //lighting
+    const ambientLight = new BABYLON.HemisphericLight(
+    "ambientLight",
+    new BABYLON.Vector3(0, 1, 0),
+    scene
+    );
+    ambientLight.intensity = 0.6;
+    ambientLight.diffuse   = new BABYLON.Color3(0.7, 0.85, 1.0);
+
+    //directional sun to simulate sun
+    const sunLight = new BABYLON.DirectionalLight(
+        "sunLight",
+        new BABYLON.Vector3(-1, -2, -1),
+        scene
+    );
+    sunLight.intensity = 0.8;
+    sunLight.positon = new BABYLON.Vector3(5, 10, -5);
+
+};
